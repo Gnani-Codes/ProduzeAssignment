5d6197741c2e51412c5562309b37aac2ecd8533a
Gnani <gnanendragariminti@gmail.com>
2023-12-17 05:12:29 +0000

    Created Header Section

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,28 @@
+import { CiCloudSun } from "react-icons/ci";
+import { FaSearch } from "react-icons/fa";
+import { FaGlobeAmericas } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+
 import "./index.css";
 
 const Header = () => (
   <div className="header-container">
-    <h1 className="header-logo">ProduzeWeather</h1>
+    <div className="logo-container">
+      <CiCloudSun id="logo" />
+      <h1 className="header-logo">ProduzeWeather</h1>
+    </div>
+
+    <div className="search-container">
+      <input type="text" placeholder="Search City" className="input" />
+      <FaSearch id="searchIcon" />
+    </div>
+
+    <div className="menu-icons-container">
+      <FaGlobeAmericas className="icons" />
+      <CgProfile className="icons" />
+      <IoMdMenu className="icons" />
+    </div>
   </div>
 );
 
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+import "./index.css";
+
+class Home extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="home-container">
+        <h1>gg</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
